96f2ba0fd1370746d697bc7e318366e843dc61ec
bui-brian <bui.brian99@gmail.com>
2025-12-06 02:30:19 -0600

    Refactor health bar and hurtbox scripts

--- scripts/health_bar.gd
+++ scripts/health_bar.gd
@@ -1,10 +1,12 @@
 extends ProgressBar
 
-@onready var unit_stats: Stats = owner.stats
+@onready var stats: Stats = owner.stats
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	unit_stats.health_changed.connect(update_health)
+	stats.health_changed.connect(update_health)
 
+# updates ProgressBar value every time Stats.health_changed signal emits
+# health, max_health are passed in as arguments
 func update_health(health, max_health):
 	value = health
--- scripts/hurtbox.gd
+++ scripts/hurtbox.gd
@@ -1,7 +1,6 @@
 class_name Hurtbox extends Area2D
 
-@onready var owner_stats: Stats = owner.stats
-@onready var owner_health_bar: ProgressBar = owner.health_bar
+@onready var stats: Stats = owner.stats
 #@onready var owner_ani_player = owner.get_node("AnimationPlayer")
 
 # Called when the node enters the scene tree for the first time.
@@ -9,17 +8,14 @@ func _ready() -> void:
 	monitoring = false
 
 func receive_hit(damage: int) -> void:
-	if not owner_stats:
+	if not stats:
 		return
 		
-	owner_stats.health -= damage
-	print(owner_stats.health)
+	stats.health -= damage # health_changed or health_depleted signal emits
+	print(stats.health)
 	
-	if owner_stats.health <= 0:
+	if stats.health <= 0:
 		#owner_ani_player.set_current_animation("death")
 		#await get_tree().create_timer(1.0).timeout
 		#owner_health_bar.queue_free()
 		owner.queue_free()
-	else:
-		owner_stats.health_changed.emit(owner_stats.health, owner_stats.base_max_health)
-		#owner_health_bar.update_health()
