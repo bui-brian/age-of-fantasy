d85f219db37f072aaed72bafa5c5b499564e34d1
bui-brian <buibrian.dev@protonmail.com>
2025-12-24 04:41:45 -0600

    Merge unit spawner class with world script and clean up

--- scripts/unit_spawner.gd
+++ scripts/unit_spawner.gd
@@ -1,15 +1,1 @@
-# spawns units facing in left/right direction given a scale_x and global position 
-
-class_name UnitSpawner extends Node2D
-
-func spawn_unit(unit_scene, unit_group: Unit.Faction, unit_direction: Vector2, unit_position: Vector2):
-	
-	var temp_scene = unit_scene
-	#temp_scene.add_to_group(unit_group)
-	temp_scene.faction = unit_group
-	temp_scene.direction = unit_direction.x
-	temp_scene.scale.x = unit_direction.x
-	temp_scene.global_position = unit_position
-	temp_scene.scale *= Vector2(1, 1)
-	
-	return temp_scene
+extends Node2D
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,12 +1,22 @@
 extends Node2D
 
-@onready var player_units = get_node("/root/World/PlayerUnits")
-@onready var enemy_units = get_node("/root/World/EnemyUnits")
-@onready var player_spawner: UnitSpawner = $PlayerSpawner
-@onready var enemy_spawner: UnitSpawner = $EnemySpawner
+@onready var unit_1_button: TextureButton = $UI/SideBar/VBoxContainer/Unit1_Button
 
 const lm_scene_path = "res://scenes/units/lightning_sorc.tscn"
 const knight_scene_path = "res://scenes/units/knight.tscn"
+
+func _ready() -> void:
+	unit_1_button.pressed.connect(_on_unit_1_button_pressed)
+
+func spawn_unit(unit_scene: Variant, unit_group: Unit.Faction, unit_direction: Vector2, unit_position: Vector2):
+	var temp_scene = unit_scene
+	temp_scene.faction = unit_group
+	temp_scene.direction = unit_direction.x
+	temp_scene.scale.x = unit_direction.x
+	temp_scene.global_position = unit_position
+	temp_scene.scale *= Vector2(1, 1)
+	
+	return temp_scene
 
 # --- Button handlers ---
 func _on_unit_1_button_pressed() -> void:
@@ -20,19 +30,19 @@ func _on_unit_1_button_pressed() -> void:
 	var knight_scene3 = preload(knight_scene_path).instantiate()
 
 	# Configure units and construct unit spawners
-	var player_spawn_mid = player_spawner.spawn_unit(lm_scene1, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, 50))
-	var player_spawn_top = player_spawner.spawn_unit(lm_scene2, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, -100))
-	var player_spawn_bot = player_spawner.spawn_unit(lm_scene3, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, 250))
+	var player_spawn_mid = spawn_unit(lm_scene1, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, 50))
+	var player_spawn_top = spawn_unit(lm_scene2, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, -100))
+	var player_spawn_bot = spawn_unit(lm_scene3, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, 250))
 	
-	var enemy_spawn_mid = enemy_spawner.spawn_unit(knight_scene1, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, 75))
-	var enemy_spawn_top = enemy_spawner.spawn_unit(knight_scene2, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, -100))
-	var enemy_spawn_bot = enemy_spawner.spawn_unit(knight_scene3, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, 250))
+	var enemy_spawn_mid = spawn_unit(knight_scene1, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, 75))
+	var enemy_spawn_top = spawn_unit(knight_scene2, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, -100))
+	var enemy_spawn_bot = spawn_unit(knight_scene3, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, 250))
 
 	# Add units to the main scene
-	player_units.add_child(player_spawn_mid)
-	player_units.add_child(player_spawn_top)
-	player_units.add_child(player_spawn_bot)
+	self.add_child(player_spawn_mid)
+	self.add_child(player_spawn_top)
+	self.add_child(player_spawn_bot)
 	
-	enemy_units.add_child(enemy_spawn_mid)
-	enemy_units.add_child(enemy_spawn_top)
-	enemy_units.add_child(enemy_spawn_bot)
+	self.add_child(enemy_spawn_mid)
+	self.add_child(enemy_spawn_top)
+	self.add_child(enemy_spawn_bot)
