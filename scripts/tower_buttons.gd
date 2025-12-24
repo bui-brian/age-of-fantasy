c726d367f2023afff94bc7c9f47c5de367417f3c
bui-brian <buibrian.dev@protonmail.com>
2025-12-24 06:40:57 -0600

    Add functionality to spawn unit at each selected tower

--- scripts/tower_buttons.gd
+++ scripts/tower_buttons.gd
@@ -4,12 +4,18 @@ extends Control
 @onready var player_tower_top_button: TextureButton = $PlayerTowerTopButton
 @onready var player_tower_bot_button: TextureButton = $PlayerTowerBotButton
 
-var locked: bool = false
+signal toggled(
+	MID_TOGGLE: bool,
+	TOP_TOGGLE: bool,
+	BOT_TOGGLE: bool,
+)
+
+var mid_toggle: bool = false
+var top_toggle: bool = false
+var bot_toggle: bool = false
+
 var tween_intensity: float = 2.5
 var tween_duration: float = 0.15
-var MID_TOGGLE: bool = false
-var TOP_TOGGLE: bool = false
-var BOT_TOGGLE: bool = false
 
 func _ready() -> void:
 	player_tower_mid_button.modulate.a = 0.0
@@ -38,57 +44,77 @@ func _ready() -> void:
 	player_tower_bot_button.pressed.connect(_on_player_tower_bot_pressed)
 
 func _on_mid_hover_entered():
-	if MID_TOGGLE:
+	if mid_toggle:
 		return
 	player_tower_mid_button.modulate.a = 1.0
 	start_tween(player_tower_mid_button, "scale", Vector2.ONE * tween_intensity, tween_duration)
 
 func _on_top_hover_entered():
-	if TOP_TOGGLE:
+	if top_toggle:
 		return
 	player_tower_top_button.modulate.a = 1.0
 	start_tween(player_tower_top_button, "scale", Vector2.ONE * tween_intensity, tween_duration)
 
 func _on_bot_hover_entered():
-	if BOT_TOGGLE:
+	if bot_toggle:
 		return
 	player_tower_bot_button.modulate.a = 1.0
 	start_tween(player_tower_bot_button, "scale", Vector2.ONE * tween_intensity, tween_duration)
 
 func _on_hover_exited():
-	if MID_TOGGLE:
+	if mid_toggle:
 		return
 	else:
 		start_tween(player_tower_mid_button, "scale", Vector2.ONE, tween_duration)
 		player_tower_mid_button.modulate.a = 0.0
 	
-	if TOP_TOGGLE:
+	if top_toggle:
 		return
 	else:
 		start_tween(player_tower_top_button, "scale", Vector2.ONE, tween_duration)
 		player_tower_top_button.modulate.a = 0.0
 	
-	if BOT_TOGGLE:
+	if bot_toggle:
 		return
 	else:
 		start_tween(player_tower_bot_button, "scale", Vector2.ONE, tween_duration)
 		player_tower_bot_button.modulate.a = 0.0
 
 func _on_player_tower_mid_pressed():
-	# Toggle locked state
-	MID_TOGGLE = !MID_TOGGLE
-	player_tower_mid_button.modulate.a = 1.0 if MID_TOGGLE else 0.0
-	player_tower_mid_button.scale = Vector2.ONE * (tween_intensity if MID_TOGGLE else 1)
+	mid_toggle = !mid_toggle
+	
+	if mid_toggle:
+		player_tower_mid_button.modulate.a = 1.0
+		player_tower_mid_button.scale = Vector2.ONE * tween_intensity
+	else:
+		player_tower_mid_button.modulate.a = 0.0
+		player_tower_mid_button.scale = Vector2.ONE * 1
+	
+	toggled.emit(mid_toggle, top_toggle, bot_toggle)
 
 func _on_player_tower_top_pressed():
-	TOP_TOGGLE = !TOP_TOGGLE
-	player_tower_top_button.modulate.a = 1.0 if TOP_TOGGLE else 0.0
-	player_tower_top_button.scale = Vector2.ONE * (tween_intensity if TOP_TOGGLE else 1)
+	top_toggle = !top_toggle
+	
+	if top_toggle:
+		player_tower_top_button.modulate.a = 1.0
+		player_tower_top_button.scale = Vector2.ONE * tween_intensity
+	else:
+		player_tower_top_button.modulate.a = 0.0
+		player_tower_top_button.scale = Vector2.ONE * 1
+	
+	toggled.emit(mid_toggle, top_toggle, bot_toggle)
 
 func _on_player_tower_bot_pressed():
-	BOT_TOGGLE = !BOT_TOGGLE
-	player_tower_bot_button.modulate.a = 1.0 if BOT_TOGGLE else 0.0
-	player_tower_bot_button.scale = Vector2.ONE * (tween_intensity if BOT_TOGGLE else 1)
+	bot_toggle = !bot_toggle
+	
+	if bot_toggle:
+		player_tower_bot_button.modulate.a = 1.0
+		player_tower_bot_button.scale = Vector2.ONE * tween_intensity
+	else:
+		player_tower_bot_button.modulate.a = 0.0
+		player_tower_bot_button.scale = Vector2.ONE * 1
+	
+	toggled.emit(mid_toggle, top_toggle, bot_toggle)
 
 func start_tween(object: Object, property: String, final_val: Variant, duration: float):
 	var tween = create_tween()
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
 @onready var unit_1_button: TextureButton = $UI/SideBar/VBoxContainer/Unit1_Button
+@onready var tower_buttons: Control = $UI/TowerButtons
 
 const lm_scene_path = "res://scenes/units/lightning_sorc.tscn"
 const knight_scene_path = "res://scenes/units/knight.tscn"
 
+var mid_toggle: bool = false
+var top_toggle: bool = false
+var bot_toggle: bool = false
+
 func _ready() -> void:
 	unit_1_button.pressed.connect(_on_unit_1_button_pressed)
+	tower_buttons.toggled.connect(_set_toggled)
 
 func spawn_unit(unit_scene: Variant, unit_group: Unit.Faction, unit_direction: Vector2, unit_position: Vector2):
 	var temp_scene = unit_scene
@@ -18,31 +24,49 @@ func spawn_unit(unit_scene: Variant, unit_group: Unit.Faction, unit_direction: V
 	
 	return temp_scene
 
+func _set_toggled(MID_TOGGLE: bool, TOP_TOGGLE: bool, BOT_TOGGLE: bool):
+	mid_toggle = MID_TOGGLE
+	top_toggle = TOP_TOGGLE
+	bot_toggle = BOT_TOGGLE
+
 # --- Button handlers ---
 func _on_unit_1_button_pressed() -> void:
-	# Preload all units
-	var lm_scene1 = preload(lm_scene_path).instantiate()
-	var lm_scene2 = preload(lm_scene_path).instantiate()
-	var lm_scene3 = preload(lm_scene_path).instantiate()
+	if mid_toggle:
+		var mid_spawn = preload(lm_scene_path).instantiate()
+		var player_spawn_mid = spawn_unit(mid_spawn, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, 50))
+		self.add_child(player_spawn_mid)
+	if top_toggle:
+		var top_spawn = preload(lm_scene_path).instantiate()
+		var player_spawn_top = spawn_unit(top_spawn, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, -100))
+		self.add_child(player_spawn_top)
+	if bot_toggle:
+		var bot_spawn = preload(lm_scene_path).instantiate()
+		var player_spawn_bot = spawn_unit(bot_spawn, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, 250))
+		self.add_child(player_spawn_bot)
 	
-	var knight_scene1 = preload(knight_scene_path).instantiate()
-	var knight_scene2 = preload(knight_scene_path).instantiate()
-	var knight_scene3 = preload(knight_scene_path).instantiate()
+	# Preload all units
+	#var lm_scene1 = preload(lm_scene_path).instantiate()
+	#var lm_scene2 = preload(lm_scene_path).instantiate()
+	#var lm_scene3 = preload(lm_scene_path).instantiate()
+	
+	#var knight_scene1 = preload(knight_scene_path).instantiate()
+	#var knight_scene2 = preload(knight_scene_path).instantiate()
+	#var knight_scene3 = preload(knight_scene_path).instantiate()
 
 	# Configure units and construct unit spawners
-	var player_spawn_mid = spawn_unit(lm_scene1, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, 50))
-	var player_spawn_top = spawn_unit(lm_scene2, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, -100))
-	var player_spawn_bot = spawn_unit(lm_scene3, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, 250))
+	#var player_spawn_mid = spawn_unit(lm_scene1, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, 50))
+	#var player_spawn_top = spawn_unit(lm_scene2, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, -100))
+	#var player_spawn_bot = spawn_unit(lm_scene3, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, 250))
 	
-	var enemy_spawn_mid = spawn_unit(knight_scene1, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, 75))
-	var enemy_spawn_top = spawn_unit(knight_scene2, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, -100))
-	var enemy_spawn_bot = spawn_unit(knight_scene3, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, 250))
+	#var enemy_spawn_mid = spawn_unit(knight_scene1, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, 75))
+	#var enemy_spawn_top = spawn_unit(knight_scene2, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, -100))
+	#var enemy_spawn_bot = spawn_unit(knight_scene3, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, 250))
 
 	# Add units to the main scene
-	self.add_child(player_spawn_mid)
-	self.add_child(player_spawn_top)
-	self.add_child(player_spawn_bot)
+	#self.add_child(player_spawn_mid)
+	#self.add_child(player_spawn_top)
+	#self.add_child(player_spawn_bot)
 	
-	self.add_child(enemy_spawn_mid)
-	self.add_child(enemy_spawn_top)
-	self.add_child(enemy_spawn_bot)
+	#self.add_child(enemy_spawn_mid)
+	#self.add_child(enemy_spawn_top)
+	#self.add_child(enemy_spawn_bot)
