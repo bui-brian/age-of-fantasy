af54a6a7866f79c1f2ea6f18be7d511b75315dfc
bui-brian <buibrian.dev@protonmail.com>
2025-12-30 03:36:55 -0600

    Refactor unit_spawner.gd by separating logic to unit button and enemy controller

--- scripts/enemy_controller.gd
+++ scripts/enemy_controller.gd
@@ -6,6 +6,10 @@ extends Node2D
 var enemy_tick_rate: float = 1.0
 var prioritize_lane: int = 0 # top = 1, mid = 2, bot = 3
 
+const sorc_scene_path = "res://scenes/units/lightning_sorc.tscn"
+const knight_scene_path = "res://scenes/units/knight.tscn"
+const dm_scene_path = "res://scenes/units/dark_magician.tscn"
+
 func _ready() -> void:
 	timer.wait_time = enemy_tick_rate
 	timer.timeout.connect(enemy_ai_tick)
@@ -30,29 +34,51 @@ func enemy_ai_tick():
 			# If there are more player units than enemy units
 			if GameState.current_player_unit_count_top > GameState.enemy_unit_count_top and GameState.enemy_gold >= 50:
 				# Spawn Lighting Sorc
-				unit_spawner.spawn_unit_top()
+				# 	Preload Sorceress scene
+				var sorc_scene = preload(sorc_scene_path).instantiate()
+				# Construct player Sorceress unit
+				var sorc_unit = unit_spawner.spawn_unit(sorc_scene, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, -100))
+				unit_spawner.spawn_unit_lane(sorc_unit)
 			elif GameState.current_player_unit_count_top < GameState.enemy_unit_count_mid:
 				pass
 			elif GameState.current_player_unit_count_top == 0 and GameState.enemy_gold >= 50:
 				# Spawn Knight
-				unit_spawner.spawn_unit_top()
+				# 	Preload Knight scene
+				var knight_scene = preload(knight_scene_path).instantiate()
+				# Construct player Knight unit
+				var knight_unit = unit_spawner.spawn_unit(knight_scene, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, -100))
+				unit_spawner.spawn_unit_lane(knight_unit)
 		2: # Mid lane prio
 			# If there are more player units than enemy units
 			if GameState.current_player_unit_count_mid > GameState.enemy_unit_count_mid and GameState.enemy_gold >= 50:
 				# Spawn Lighting Sorc
-				unit_spawner.spawn_unit_mid()
+				# 	Preload Sorceress scene
+				var sorc_scene = preload(sorc_scene_path).instantiate()
+				# Construct player Sorceress unit
+				var sorc_unit = unit_spawner.spawn_unit(sorc_scene, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, 50))
+				unit_spawner.spawn_unit_lane(sorc_unit)
 			elif GameState.current_player_unit_count_mid < GameState.enemy_unit_count_mid:
 				pass
 			elif GameState.current_player_unit_count_mid == 0 and GameState.enemy_gold >= 50:
 				# Spawn Knight
-				unit_spawner.spawn_unit_mid()
+				var knight_scene = preload(knight_scene_path).instantiate()
+				# Construct player Knight unit
+				var knight_unit = unit_spawner.spawn_unit(knight_scene, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, 50))
+				unit_spawner.spawn_unit_lane(knight_unit)
 		3: # Bot lane prio
 			if GameState.current_player_unit_count_bot > GameState.enemy_unit_count_bot and GameState.enemy_gold >= 50:
 				# Spawn Lighting Sorc
-				unit_spawner.spawn_unit_bot()
+				# 	Preload Sorceress scene
+				var sorc_scene = preload(sorc_scene_path).instantiate()
+				# Construct player Sorceress unit
+				var sorc_unit = unit_spawner.spawn_unit(sorc_scene, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, 250))
+				unit_spawner.spawn_unit_lane(sorc_unit)
 			elif GameState.current_player_unit_count_bot < GameState.enemy_unit_count_bot:
 				pass
 			elif GameState.current_player_unit_count_bot == 0 and GameState.enemy_gold >= 50:
 				# Spawn Knight
-				unit_spawner.spawn_unit_bot()
+				var knight_scene = preload(knight_scene_path).instantiate()
+				# Construct player Knight unit
+				var knight_unit = unit_spawner.spawn_unit(knight_scene, Unit.Faction.ENEMY, Vector2.LEFT, Vector2(750, 250))
+				unit_spawner.spawn_unit_lane(knight_unit)
 	
--- scripts/sorceress_button.gd
+++ scripts/sorceress_button.gd
@@ -1,10 +1,44 @@
 extends TextureButton
 
-@onready var sorc_spawner: UnitSpawner = $"../../../../UnitSpawner"
+@onready var unit_spawner: UnitSpawner = $"../../../../UnitSpawner"
+@onready var tower_buttons: Control = $"../../../TowerButtons"
+
+const sorc_scene_path = "res://scenes/units/lightning_sorc.tscn"
+
+var mid_toggle: bool = false
+var top_toggle: bool = false
+var bot_toggle: bool = false
 
 func _ready() -> void:
 	self.pressed.connect(_sorceress_button_pressed)
+	tower_buttons.toggled.connect(_set_toggled)
 
 # --- Button handlers ---
 func _sorceress_button_pressed() -> void:
-	sorc_spawner.spawn_unit_lane()
+	#sorc_spawner.spawn_unit_lane()
+	# If toggles are on, spawn in respective lane
+	if mid_toggle:
+		# Preload Sorceress scene
+		var sorc_scene = preload(sorc_scene_path).instantiate()
+		# Construct player Sorceress unit
+		var sorc_unit = unit_spawner.spawn_unit(sorc_scene, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, 50))
+		unit_spawner.spawn_unit_lane(sorc_unit)
+		
+	if top_toggle:
+		# Preload Sorceress scene
+		var sorc_scene = preload(sorc_scene_path).instantiate()
+		# Construct player Sorceress unit
+		var sorc_unit = unit_spawner.spawn_unit(sorc_scene, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, -100))
+		unit_spawner.spawn_unit_lane(sorc_unit)
+	if bot_toggle:
+		# Preload Sorceress scene
+		var sorc_scene = preload(sorc_scene_path).instantiate()
+		# Construct player Sorceress unit
+		var sorc_unit = unit_spawner.spawn_unit(sorc_scene, Unit.Faction.PLAYER, Vector2.RIGHT, Vector2(-750, 250))
+		unit_spawner.spawn_unit_lane(sorc_unit)
+	
+
+func _set_toggled(MID_TOGGLE: bool, TOP_TOGGLE: bool, BOT_TOGGLE: bool) -> void:
+	mid_toggle = MID_TOGGLE
+	top_toggle = TOP_TOGGLE
+	bot_toggle = BOT_TOGGLE
