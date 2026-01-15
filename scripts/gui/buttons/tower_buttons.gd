54cdf0c9a3354fcfa0d28157a5a927bb5994afce
bui-brian <buibrian.dev@protonmail.com>
2026-01-15 04:02:00 -0600

    Fix bug where the hover effect will persist on player Towers if one is selected

--- scripts/gui/buttons/tower_buttons.gd
+++ scripts/gui/buttons/tower_buttons.gd
@@ -1,7 +1,7 @@
 class_name TowerButtons extends Control
 
-@onready var player_tower_mid_button: TextureButton = $PlayerTowerMidButton
 @onready var player_tower_top_button: TextureButton = $PlayerTowerTopButton
+@onready var player_tower_mid_button: TextureButton = $PlayerTowerMidButton
 @onready var player_tower_bot_button: TextureButton = $PlayerTowerBotButton
 
 signal toggled(
@@ -10,38 +10,50 @@ signal toggled(
 	BOT_TOGGLE: bool,
 )
 
-var mid_toggle: bool = false
 var top_toggle: bool = false
+var mid_toggle: bool = false
 var bot_toggle: bool = false
 
 var tween_intensity: float = 2.5
 var tween_duration: float = 0.15
 
 func _ready() -> void:
+	player_tower_top_button.modulate.a = 0.0
+	player_tower_top_button.pivot_offset = player_tower_top_button.size / 2
+	player_tower_top_button.mouse_filter = Control.MOUSE_FILTER_STOP
+	
 	player_tower_mid_button.modulate.a = 0.0
 	player_tower_mid_button.pivot_offset = player_tower_mid_button.size / 2
 	player_tower_mid_button.mouse_filter = Control.MOUSE_FILTER_STOP
 	
-	player_tower_top_button.modulate.a = 0.0
-	player_tower_top_button.pivot_offset = player_tower_mid_button.size / 2
-	player_tower_top_button.mouse_filter = Control.MOUSE_FILTER_STOP
-	
 	player_tower_bot_button.modulate.a = 0.0
-	player_tower_bot_button.pivot_offset = player_tower_mid_button.size / 2
+	player_tower_bot_button.pivot_offset = player_tower_bot_button.size / 2
 	player_tower_bot_button.mouse_filter = Control.MOUSE_FILTER_STOP
 
 	# Connect signals
-	player_tower_mid_button.mouse_entered.connect(_on_mid_hover_entered)
-	player_tower_mid_button.mouse_exited.connect(_on_hover_exited)
-	player_tower_mid_button.pressed.connect(_on_player_tower_mid_pressed)
-	
 	player_tower_top_button.mouse_entered.connect(_on_top_hover_entered)
-	player_tower_top_button.mouse_exited.connect(_on_hover_exited)
+	player_tower_top_button.mouse_exited.connect(_on_top_hover_exited)
 	player_tower_top_button.pressed.connect(_on_player_tower_top_pressed)
 	
+	player_tower_mid_button.mouse_entered.connect(_on_mid_hover_entered)
+	player_tower_mid_button.mouse_exited.connect(_on_mid_hover_exited)
+	player_tower_mid_button.pressed.connect(_on_player_tower_mid_pressed)
+	
 	player_tower_bot_button.mouse_entered.connect(_on_bot_hover_entered)
-	player_tower_bot_button.mouse_exited.connect(_on_hover_exited)
+	player_tower_bot_button.mouse_exited.connect(_on_bot_hover_exited)
 	player_tower_bot_button.pressed.connect(_on_player_tower_bot_pressed)
+
+# Start tween animation
+func start_tween(object: Object, property: String, final_val: Variant, duration: float):
+	var tween = create_tween()
+	tween.tween_property(object, property, final_val, duration)
+
+# --- on Tower hover entered ---
+func _on_top_hover_entered():
+	if top_toggle:
+		return
+	player_tower_top_button.modulate.a = 1.0
+	start_tween(player_tower_top_button, "scale", Vector2.ONE * tween_intensity, tween_duration)
 
 func _on_mid_hover_entered():
 	if mid_toggle:
@@ -49,36 +61,43 @@ func _on_mid_hover_entered():
 	player_tower_mid_button.modulate.a = 1.0
 	start_tween(player_tower_mid_button, "scale", Vector2.ONE * tween_intensity, tween_duration)
 
-func _on_top_hover_entered():
-	if top_toggle:
-		return
-	player_tower_top_button.modulate.a = 1.0
-	start_tween(player_tower_top_button, "scale", Vector2.ONE * tween_intensity, tween_duration)
-
 func _on_bot_hover_entered():
 	if bot_toggle:
 		return
 	player_tower_bot_button.modulate.a = 1.0
 	start_tween(player_tower_bot_button, "scale", Vector2.ONE * tween_intensity, tween_duration)
 
-func _on_hover_exited():
-	if mid_toggle:
-		return
-	else:
-		start_tween(player_tower_mid_button, "scale", Vector2.ONE, tween_duration)
-		player_tower_mid_button.modulate.a = 0.0
-	
+# --- on Tower hover exited ---
+func _on_top_hover_exited():
 	if top_toggle:
 		return
-	else:
-		start_tween(player_tower_top_button, "scale", Vector2.ONE, tween_duration)
-		player_tower_top_button.modulate.a = 0.0
+	player_tower_top_button.modulate.a = 0.0
+	start_tween(player_tower_top_button, "scale", Vector2.ONE, tween_duration)
 	
+func _on_mid_hover_exited():
+	if mid_toggle:
+		return
+	player_tower_mid_button.modulate.a = 0.0
+	start_tween(player_tower_mid_button, "scale", Vector2.ONE, tween_duration)
+	
+func _on_bot_hover_exited():
 	if bot_toggle:
 		return
+	player_tower_bot_button.modulate.a = 0.0
+	start_tween(player_tower_bot_button, "scale", Vector2.ONE, tween_duration)
+
+# --- on Tower pressed ---
+func _on_player_tower_top_pressed():
+	top_toggle = !top_toggle
+	
+	if top_toggle:
+		player_tower_top_button.modulate.a = 1.0
+		player_tower_top_button.scale = Vector2.ONE * tween_intensity
 	else:
-		start_tween(player_tower_bot_button, "scale", Vector2.ONE, tween_duration)
-		player_tower_bot_button.modulate.a = 0.0
+		player_tower_top_button.modulate.a = 0.0
+		player_tower_top_button.scale = Vector2.ONE * 1
+	
+	toggled.emit(mid_toggle, top_toggle, bot_toggle)
 
 func _on_player_tower_mid_pressed():
 	mid_toggle = !mid_toggle
@@ -89,18 +108,6 @@ func _on_player_tower_mid_pressed():
 	else:
 		player_tower_mid_button.modulate.a = 0.0
 		player_tower_mid_button.scale = Vector2.ONE * 1
-	
-	toggled.emit(mid_toggle, top_toggle, bot_toggle)
-
-func _on_player_tower_top_pressed():
-	top_toggle = !top_toggle
-	
-	if top_toggle:
-		player_tower_top_button.modulate.a = 1.0
-		player_tower_top_button.scale = Vector2.ONE * tween_intensity
-	else:
-		player_tower_top_button.modulate.a = 0.0
-		player_tower_top_button.scale = Vector2.ONE * 1
 	
 	toggled.emit(mid_toggle, top_toggle, bot_toggle)
 
@@ -115,7 +122,3 @@ func _on_player_tower_bot_pressed():
 		player_tower_bot_button.scale = Vector2.ONE * 1
 	
 	toggled.emit(mid_toggle, top_toggle, bot_toggle)
-
-func start_tween(object: Object, property: String, final_val: Variant, duration: float):
-	var tween = create_tween()
-	tween.tween_property(object, property, final_val, duration)
