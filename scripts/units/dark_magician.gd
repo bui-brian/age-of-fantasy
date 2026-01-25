84344170421e2df387ff14859e72e222ac2ba0e2
bui-brian <buibrian.dev@protonmail.com>
2026-01-25 03:11:17 -0600

    Small gameplay balance

--- scripts/units/dark_magician.gd
+++ scripts/units/dark_magician.gd
@@ -1,5 +1,8 @@
 class_name DarkMagician extends Unit
 
 func _ready():
-	stats.base_damage = 40
+	stats.base_damage = 25
+	health.current_max_health = 120
+	health_bar.max_value = health.current_max_health
+	
 	state_machine.init(self)
--- scripts/units/lightning_sorc.gd
+++ scripts/units/lightning_sorc.gd
@@ -1,5 +1,8 @@
 class_name LM extends Unit
 
 func _ready():
-	stats.base_damage = 40
+	stats.base_damage = 60
+	health.current_max_health = 100
+	health_bar.max_value = health.current_max_health
+	
 	state_machine.init(self)
